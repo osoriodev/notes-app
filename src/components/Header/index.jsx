aae993f964515c5ed1d1b81f8ac9e7898305a7e2
David Osorio <osoriodev@outlook.com>
2022-09-20 18:01:29 -0500

    Update Header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,29 @@
-import { BiPlus, BiSearch } from 'react-icons/bi';
+import { BiSearch } from 'react-icons/bi';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <h1>Notes App</h1>
-      <div className={styles.content}>
-        <div className={styles.finder}>
-          <BiSearch className={styles.searchIcon} />
-          <input
-            className={styles.input}
-            type="search"
-            name="finder"
-            id="search"
-            placeholder="Search note..."
-          />
-        </div>
-        <button
-          className={styles.btn}
-          aria-label="Create Note"
-        >
-          <BiPlus className={styles.addIcon} />
-        </button>
+      <div className={styles.finder}>
+        <BiSearch
+          className={styles.searchIcon}
+          size="1.5rem"
+          aria-hidden="true"
+        />
+        <input
+          className={styles.input}
+          type="search"
+          name="finder"
+          id="search"
+          placeholder="Search note..."
+        />
       </div>
+      <button
+        className={styles.btn}
+      >
+        Create note
+      </button>
     </header>
   );
 }
